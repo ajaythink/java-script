d49f3a0593c9408b540ba2ffee87bf85800da97d
Ajay Kumar <104864110+ajaythink@users.noreply.github.com>
2023-12-22 19:16:34 +0000

    conversion operation and comparison commit

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,27 +1,49 @@
 let score = "33"
-console.log(typeof score) //number type
-console.log(typeof (score)) //number type
+// console.log(typeof score) //number type
+// console.log(typeof (score)) //number type
 
 
 let valueInNumber = Number(score) //type casting
-console.log(typeof valueInNumber)
+// console.log(typeof valueInNumber)
 
-console.log(valueInNumber) // o/p 33
+// console.log(valueInNumber) // o/p 33
 
 let score1 = "33abc"
 let valueInNumber1 = Number(score1)
-console.log(typeof valueInNumber1)
-console.log(valueInNumber1) // NaN = Not a Number
+// console.log(typeof valueInNumber1)
+// console.log(valueInNumber1) // NaN = Not a Number
 
 //if value will null assign
 let score2 = null
 let valueInNumber2 = Number( score2)
-console.log(valueInNumber2)
-console.log(typeof valueInNumber2)
+// console.log(valueInNumber2)
+// console.log(typeof valueInNumber2)
 
 //boolean
-let booleanLoged = "Ajay"
+let booleanLoged = 0
 let booleanIsLogedIn = Boolean(booleanLoged)
-console.log(typeof booleanIsLogedIn)
+// console.log(typeof booleanIsLogedIn)
+// console.log(booleanIsLogedIn)
 
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
 
+// *************Operator************
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2 + 3)
+// console.log( 2-3)
+// console.log( 2* 3)
+// console.log(2**3)
+// console.log(2/4)
+// console.log(2%3)
+
+// console.log("ajay" + "Kumar")
+// console.log("1" + 2)
+// console.log(2 + "2")
+// console.log("1" + 2 + 2)
+// console.log(2 + 3 + "2")
