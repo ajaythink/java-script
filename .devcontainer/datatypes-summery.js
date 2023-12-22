dead3febe46702557d026937934fa5d4b5e3880d
Ajay Kumar <104864110+ajaythink@users.noreply.github.com>
2023-12-22 19:36:59 +0000

    data type summery

---
+++ .devcontainer/datatypes-summery.js
@@ -0,0 +1,31 @@
+//Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, symbol,
+//BigInt
+const score = 100
+const scoreValue = 200.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+//Regerence type( Non Primitive)
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "ajay",
+    age: 22,
+}
+const myfuncation = function(){
+    console.log("Hello World!");
+}
+console.log(typeof myfuncation)
+console.log(typeof heros);
+
+//ecma website 
