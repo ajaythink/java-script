e2a54dbf8aa27a90552cf1484465b835e93f26a3
Ajay Kumar <104864110+ajaythink@users.noreply.github.com>
2023-12-25 07:31:53 +0000

    String all used

---
+++ .devcontainer/01_basics/05_string.js
@@ -0,0 +1,24 @@
+const name = "ajay"
+// const repoCount = 50
+
+// console.log(name + repoCount + " Value");// old model
+// String pulation
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);// new model
+
+//String Declare
+const gameName = new String('ajaykr');
+// key value pair access;
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);  //o/p {} type of OBJECT
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());  //Non Primitive type example
+// console.log(gameName.charAt('3'));
+// console.log(gameName.indexOf('k'));
+
+// const newString = gameName.substring(0,3);
+// console.log(newString);
+
+const anotherString = gameName.slice(-3, 1)
+console.log(anotherString);
+
