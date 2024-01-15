8f829cc6f0ad3ef665b40950b37403548a7f918e
Ajay Kumar <104864110+ajaythink@users.noreply.github.com>
2024-01-15 11:18:07 +0000

    function scope, function store in variable at time and access it.

--- 03_objects/02_scopes.js
+++ 03_objects/02_scopes.js
@@ -15,3 +15,50 @@ var c = 4000
 // console.log(b)   //ReferenceError: b is not defined
 console.log(c)      //30        c value is printing from block
 console.log(d)      //40 without declear varible 
+
+// 15/1/2024
+// video number 20
+// ******Nested Scope***
+function one(){
+    const username = "Ajay"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);       // We can not access it, website scope is not access out of the block
+    
+    two()                       
+}
+// one()
+
+if(true){
+    const username = "ajay"
+    if(username === "ajay"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);           // scope is not out of inner if condition
+}
+// console.log(username);
+
+//+++++++++++ Interesting ++++++++ Function Decleration ++++
+function addone(num){
+    return num + 1;
+}
+console.log(addone(5))
+
+console.log(addone1(5))
+function addone1(num){
+    return num + 1;
+}
+
+const addTwo = function(num){
+    return num + 2;
+}
+console.log(addTwo(6))
+
+console.log(addTwo1(6))         //ReferenceError: Cannot access 'addTwo1' before initialization
+const addTwo1 = function(num){
+    return num + 2;
+}
