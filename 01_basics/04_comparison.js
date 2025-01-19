450ee78c5adbbf08b70f8214ab87771695214c98
Ajay Kumar <ajaykumar286458@gmail.com>
2025-01-19 10:52:07 +0530

    hi

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,7 +1,7 @@
 //console.log(2 > 1);
 //console.log(2 >= 1);
-//console.log( 2 == 1);
-//console.log(2 != 1);
+// console.log( 2 == 1);
+// console.log(2 != 1);
 
 console.log("2" > 1);
 console.log("02" > 1);
